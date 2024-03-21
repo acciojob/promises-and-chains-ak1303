998837bbde207382511feca9bab6fb2f80e92b5f
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-03-21 16:37:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let submitBtn = document.getElementById('btn');
+let nameInput = document.getElementById('name');
+let ageInput = document.getElementById('age');
+
+submitBtn.addEventListener('click',(e)=>{
+	e.preventDefault();
+	let name = nameInput.value;
+	let age = ageInput.value;
+	let promise = new Promise((resolve,reject)=>{ 
+		if(age>18)
+			setTimeout(()=>{
+				resolve(`Welcome, ${name}. You can vote.`);
+			},4000);
+		else reject(`Oh sorry ${name}. You aren't old enough.`);
+	});
+	if(name!="" && age!=""){
+		promise.then((data)=>{
+			alert(data)
+		}).catch((data)=>alert(data));
+	}
+})
